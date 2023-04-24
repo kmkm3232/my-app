74b10a08cfb9116bbc0912ab505a61d1fdc73d6d
unknown <kfng57@gmail.com>
2023-04-24 18:20:49 +0800

    collapse menu

--- src/component/table/index.tsx
+++ src/component/table/index.tsx
@@ -1,30 +1,76 @@
 import React from 'react';
 
+
 export const Table = () => {
   
     return (
         <div>
-<table className="min-w-full divide-y divide-gray-200">
-  <thead className="bg-gray-50">
-    <tr>
-      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
-      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Role</th>
-    </tr>
-  </thead>
-  <tbody className="bg-white divide-y divide-gray-200">
-    <tr>
-      <td className="px-6 py-4 whitespace-nowrap"><div className="text-sm font-medium text-gray-900">John Doe</div></td>
-      <td className="px-6 py-4 whitespace-nowrap"><div className="text-sm text-gray-500">johndoe@example.com</div></td>
-      <td className="px-6 py-4 whitespace-nowrap"><div className="text-sm text-gray-500">Admin</div></td>
-    </tr>
-    <tr>
-      <td className="px-6 py-4 whitespace-nowrap"><div className="text-sm font-medium text-gray-900">Jane Smith</div></td>
-      <td className="px-6 py-4 whitespace-nowrap"><div className="text-sm text-gray-500">janesmith@example.com</div></td>
-      <td className="px-6 py-4 whitespace-nowrap"><div className="text-sm text-gray-500">User</div></td>
-    </tr>
-  </tbody>
-</table>
+
+<div className="relative overflow-x-auto">
+    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" className="px-6 py-3">
+                    Product name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Color
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Category
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Price
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Apple MacBook Pro 17"
+                </th>
+                <td className="px-6 py-4">
+                    Silver
+                </td>
+                <td className="px-6 py-4">
+                    Laptop
+                </td>
+                <td className="px-6 py-4">
+                    $2999
+                </td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Microsoft Surface Pro
+                </th>
+                <td className="px-6 py-4">
+                    White
+                </td>
+                <td className="px-6 py-4">
+                    Laptop PC
+                </td>
+                <td className="px-6 py-4">
+                    $1999
+                </td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Magic Mouse 2
+                </th>
+                <td className="px-6 py-4">
+                    Black
+                </td>
+                <td className="px-6 py-4">
+                    Accessories
+                </td>
+                <td className="px-6 py-4">
+                    $99
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
         </div>
     );
   };
